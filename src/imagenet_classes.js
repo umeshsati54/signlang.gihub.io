02a30e21e7747de337f928627938e4e3b28fa03d
softmax <umeshsati54@gmail.com>
2019-03-16 20:20:36 +0530

    with local paths

---
+++ src/imagenet_classes.js
@@ -0,0 +1,44 @@
+// from: https://github.com/hideya/tfjs-examples/blob/master/mobilenet/imagenet_classes.js
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+export const IMAGENET_CLASSES = {
+  0: 'a',
+  1: 'b',
+  2: 'c',
+  3: 'd',
+  4: 'e',
+  5: 'f',
+  6: 'g',
+  7: 'h',
+  8: 'i',
+  9: 'k',
+  10: 'l',
+  11: 'm',
+  12: 'n',
+  13: 'o',
+  14: 'p',
+  15: 'q',
+  16: 'r',
+  17: 's',
+  18: 't',
+  19: 'u',
+  20: 'v',
+  21: 'w',
+  22: 'x',
+  23: 'y',
+};
