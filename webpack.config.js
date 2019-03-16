b147ae4dff7e5f6d4dbdf45512d0ff2f4b767f3b
softmax <umeshsati54@gmail.com>
2019-03-16 20:57:07 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: 'https://raw.githubusercontent.com/umeshsati54/umeshsati54.github.io/master/src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
